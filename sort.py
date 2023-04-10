9c18b46bed60cf1597894a77cd08563d19270476
Samyuktha B R <118185674+Samyukthabr@users.noreply.github.com>
2023-04-10 11:51:15 +0530

    Delete sort.py

--- sort.py
+++
@@ -1,8 +0,0 @@
-names = input("Enter a list of names, separated by spaces: ")
-name_list = names.split()
-
-sorted_names = sorted(name_list)
-
-
-for name in sorted_names:
-    print("sorted list:",name)
