343614fab5624c9badfcc9c819bcb1f63f9bb195
Samyuktha B R <118185674+Samyukthabr@users.noreply.github.com>
2023-04-10 11:41:15 +0530

    Delete 3rd.py

--- 3rd.py
+++
@@ -1,8 +0,0 @@
-#take the input from the user
-strings = input("Enter a list of strings:").split()
-
-#sort the string according to alphabetical order
-sorted_strings = sorted(strings)
-
-#print the sorted string
-print(sorted_strings)
