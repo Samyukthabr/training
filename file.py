6381d2eb2e01ab934d256b955e55a010de483db6
Samyuktha B R <118185674+Samyukthabr@users.noreply.github.com>
2023-04-10 11:48:11 +0530

    Delete file.py

--- file.py
+++
@@ -1,9 +0,0 @@
-while True:
-    try:
-        filename = input("Enter a filename: ")
-        with open(filename) as file:
-            contents = file.read()
-            print(contents)
-        break
-    except FileNotFoundError:
-        print(f"Error: {filename} not found. Please enter a valid filename.")
